be1bec2fdc75456b390fca877df0e36ec5293f67
Rasmus Midjord <81384626+lidtmeget@users.noreply.github.com>
2023-05-04 14:10:24 +0200

    File upload

---
+++ last_visited_post.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Last Visited Post
+Description: Displays the title and a link to the last visited post for a user.
+Version: 1.0
+Author: Rasmus Midjord
+Author URI: https://www.lidtmeget.dk
+License: GPL2
+*/
+
+// Save the last visited post ID for the current user
+function save_last_visited_post() {
+    global $post;
+    if (is_singular('post')) { // Check if the current post type is "post"
+        $user_id = get_current_user_id();
+        $post_id = $post->ID;
+        update_user_meta($user_id, 'last_visited_post_id', $post_id);
+    }
+}
+add_action('wp', 'save_last_visited_post');
+
+// Display a link to the last visited post for the current user via shortcode
+function last_visited_post_shortcode($atts) {
+    $user_id = get_current_user_id();
+    $post_id = get_user_meta($user_id, 'last_visited_post_id', true);
+    $link = get_permalink($post_id);
+    $post_title = get_the_title($post_id);
+    $post_excerpt = wp_trim_words(get_the_excerpt($post_id), 20, '...');
+    return '<div class="last_visited_post"><a href="' . $link . '"><h3>' . $post_title . '</h3>' . $post_excerpt . '</a></div>';
+}
+add_shortcode('last_visited_post', 'last_visited_post_shortcode');
